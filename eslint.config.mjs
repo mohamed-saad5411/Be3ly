4a4b8c67f0d7b206d872f60035dd87557bbc677d
mohamed-saad5411 <124407370+mohamed-saad5411@users.noreply.github.com>
2026-01-17 23:03:45 +0200

    fix eslint tanstack query

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc"
-
-
+import { FlatCompat } from "@eslint/eslintrc";
+import tanstackQuery from "@tanstack/eslint-plugin-query";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,23 +10,55 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-// const eslintConfig = [
-//   ...compat.extends("next/core-web-vitals", "next/typescript"),
-// ];
-
-
-
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 const configs = [
   ...compat.extends("next/core-web-vitals"),
   ...compat.extends("next/typescript"),
-  ...compat.extends("plugin:@tanstack/eslint-plugin-query/recommended"),
   ...compat.extends("prettier"),
-  ...compat.plugins("@tanstack/query"),
-]
 
-export default configs
+  {
+    plugins: {
+      "@tanstack/query": tanstackQuery,
+    },
+    rules: {
+      ...tanstackQuery.configs.recommended.rules,
+    },
+  },
+];
+
+export default configs;
+
+
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc"
+
+
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// // const eslintConfig = [
+// //   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// // ];
+
+
+
+
+// /** @type {import('eslint').Linter.Config[]} */
+// const configs = [
+//   ...compat.extends("next/core-web-vitals"),
+//   ...compat.extends("next/typescript"),
+//   ...compat.extends("plugin:@tanstack/eslint-plugin-query/recommended"),
+//   ...compat.extends("prettier"),
+//   ...compat.plugins("@tanstack/query"),
+// ]
+
+// export default configs
 
 // const eslintConfig = [
 //   ...compat.extends("next/core-web-vitals", "next/typescript", {
